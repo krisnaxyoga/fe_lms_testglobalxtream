9f79a7775f64dd3b58d55086f9b334d12ff4cb5b
muliadev3 <softhouse3@themulia.com>
2024-07-22 17:20:18 +0800

    Update: repairing bug daterange

--- src/components/Daterange.jsx
+++ src/components/Daterange.jsx
@@ -1,30 +1,32 @@
+// CustomDateRangePicker.js
 import React, { useState } from 'react';
 import { DateRangePicker } from 'react-date-range';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 
-function CustomDateRangePicker() {
+function CustomDateRangePicker({ onChange }) {
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
     const [showPicker, setShowPicker] = useState(false);
-    const [dateSelectionState, setDateSelectionState] = useState('start'); // state to track whether selecting start or end date
+    const [dateSelectionState, setDateSelectionState] = useState('start');
 
     const handleSelect = (ranges) => {
         if (dateSelectionState === 'start') {
             setStartDate(ranges.selection.startDate);
             setEndDate(ranges.selection.endDate);
-            setDateSelectionState('end'); // set state to 'end' after selecting start date
+            setDateSelectionState('end');
         } else {
             setEndDate(ranges.selection.endDate);
-            setShowPicker(false); // hide picker after selecting end date
-            setDateSelectionState('start'); // reset state to 'start' for next selection
+            setShowPicker(false);
+            setDateSelectionState('start');
+            onChange(ranges.selection); // Pass selected range to parent component
         }
     };
 
     const togglePicker = () => {
         setShowPicker(!showPicker);
         if (!showPicker) {
-            setDateSelectionState('start'); // reset state when opening picker
+            setDateSelectionState('start');
         }
     };
 
